094bc145bfda0f01eb6e3c2184b2fdd424120008
Carsten Elton Sørensen <csoren@gmail.com>
2015-02-03 13:50:11 +0100

    Fixed 68k tests for OSX

--- test/680x0/run.sh
+++ test/680x0/run.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 function test {
-	echo Testing $i
+	echo Testing $1
 	../../build/scons/build_680x0/motor68k -f$2 -o$1.bin $1 >$1.out 2>&1
-    od -t x1 $1.bin >$1.r
+    od -t x1 $1.bin | sed 's/  */ /g' | sed -e '$a\' >$1.r
 	cat $1.r $1.out >$1.output 2>/dev/null
-	rm $1.r $1.out 2>/dev/null
+	rm $1.bin $1.r $1.out 2>/dev/null
 	diff $1.output $1.answer
 	if [ $? -eq 0 ]; then
 		rm $1.output
 	fi
 }
+
+function testlink {
+	echo Testing $i
+	../../build/scons/build_680x0/motor68k -o$1.obj $1 >$1.out 2>&1
+	../../build/scons/build_xlink/xlink -o$1.bin
+    od -t x1 $1.bin | sed 's/  */ /g' | sed -e '$a\' >$1.r
+	cat $1.r $1.out >$1.output 2>/dev/null
+	rm $.bin $1.r $1.out 2>/dev/null
+	diff $1.output $1.answer
+	if [ $? -eq 0 ]; then
+		rm $1.output
+	fi
+}
+
+
 
 test amigaexe.68k g
 test amigaobj.68k h
