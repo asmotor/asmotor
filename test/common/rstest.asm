1ecf8252cf997f488d6920cd179648cc50f3c445
Carsten Elton Sorensen <csoren@gmail.com>
2019-02-23 16:53:58 +0100

    RS test promoted

--- test/common/rstest.asm
+++ test/common/rstest.asm
@@ -1,26 +1,28 @@
-	SECTION	"John",CODE
+	OPT	el
+
+	SECTION	"Test",CODE[0]
 
 Scope:
 
 	RSSET	30
-Test1	__RSB	1
+Test1	__RSW	1
 Test2	__RSW	1
-Test3	__RSW	4
-Test_SIZEOF	RB	0
+Test3	__RSL	4
+Test_SIZEOF	__RSB	0
 
 	PRINTT	"Test1 (should be $1E): "
 	PRINTV	Test1
 	PRINTT	"\n"
 
-	PRINTT	"Test2 (should be $1F): "
+	PRINTT	"Test2 (should be $20): "
 	PRINTV	Test2
 	PRINTT	"\n"
 
-	PRINTT	"Test3 (should be $21): "
+	PRINTT	"Test3 (should be $22): "
 	PRINTV	Test3
 	PRINTT	"\n"
 
-	PRINTT	"Test_SIZEOF (should be $29): "
+	PRINTT	"Test_SIZEOF (should be $32): "
 	PRINTV	Test_SIZEOF
 	PRINTT	"\n"
 
@@ -36,6 +38,3 @@ TestMacro:	MACRO
 	TestMacro
 	TestMacro
 	PRINTT	"\n"
-
-	INCBIN	"rstest.asm"
-
