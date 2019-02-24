d852917134f6061e256c125c2af2403646c0e817
Carsten Elton Sorensen <csoren@gmail.com>
2019-02-24 11:43:17 +0100

    Removed Game Boy examples

--- test/gameboy/irqtest/m.bat
+++
@@ -1,3 +0,0 @@
-..\..\..\..\Win32Release\motorgb -oirqtest.obj -i..\minios\include\ irqtest.asm
-..\..\..\..\Win32Release\motorlink -ogbirq.gb -tg irqtest.obj
-..\..\..\..\Win32Release\motorgbfix -p -v gbirq.gb
--- test/gameboy/minios/m.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-MYASM="../../../build/cmake/debug/xasm/z80/motorz80 -mcg -iinclude/ -z0"
-MYLIB="../../../build/cmake/debug/xlib/xlib"
-
-$MYASM -oirq.obj irq.asm
-$MYASM -outility.obj utility.asm
-
-$MYLIB mos.lib a irq.obj utility.obj
