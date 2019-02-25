4d5749fd5af067d1911d438073fcf1af6d61e11c
Carsten Elton Sorensen <csoren@gmail.com>
2019-02-25 21:35:43 +0100

    Updated configuration for MSVC

--- b.ps1
+++ b.ps1
@@ -1,1 +1,2 @@
 cmake --build build/cmake/debug -j 14
+#cmake --build build/cmake/release --config Release -j 14 --target install
--- init.ps1
+++ init.ps1
@@ -1,3 +1,3 @@
 Remove-Item -Recurse build/cmake
 cmake -DCMAKE_BUILD_TYPE=Debug -S . -B build/cmake/debug
-cmake -DCMAKE_BUILD_TYPE=Release -S . -B build/cmake/release
+cmake -DCMAKE_INSTALL_PREFIX=bin -DCMAKE_BUILD_TYPE=Release -S . -B build/cmake/release
