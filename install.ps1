e7681a60aaf73675f6610e15811f7f3049a71a94
quorum <micjph@protonmail.com>
2020-11-05 10:01:09 +0100

    Windows install script added

---
+++ install.ps1
@@ -0,0 +1,13 @@
+#!/bin/sh
+$DESTDIR="$home"
+
+mkdir "$DESTDIR\bin"
+
+Remove-Item 'build/windows/release' -Recurse
+mkdir 'build/windows/release'
+
+Set-Location 'build/windows/release'
+cmake -D ASMOTOR_VERSION=$(Get-Content ..\..\..\build\version) -D CMAKE_INSTALL_PREFIX=$DESTDIR --config Release ..\..\..
+Set-Location '..\..\..'
+
+cmake --build build/windows/release --target install --config Release
