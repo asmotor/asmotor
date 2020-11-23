ace8da69a6ab198cbb376043e506196ba9274f77
quorum <micjph@protonmail.com>
2020-11-23 09:20:04 +0100

    Windows install script fix

--- install.ps1
+++ install.ps1
@@ -10,4 +10,5 @@ Set-Location 'build/windows/release'
 cmake -D ASMOTOR_VERSION=$(Get-Content ..\..\..\build\version) -D CMAKE_INSTALL_PREFIX=$DESTDIR --config Release ..\..\..
 Set-Location '..\..\..'
 
-cmake --build build/windows/release --target install --config Release
+cmake --build build\windows\release --config Release
+cmake --install build\windows\release --config Release
