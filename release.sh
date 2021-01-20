fc568c77a47c0bf5db820addd73c68f7aafd768f
quorum <micjph@protonmail.com>
2021-01-20 13:06:01 +0100

    Release script fix

--- release.sh
+++ release.sh
@@ -3,5 +3,5 @@ git tag $1
 cd build
 echo -n $1 >version
 ./build_source.sh
-gh release create "$1" "asmotor-$1-src.*" -d -n "Version $1" -p -t "Version $1"
+gh release create "$1" asmotor-$1-src.* -d -n "Version $1" -p -t "Version $1"
 echo -n $1.next >version
