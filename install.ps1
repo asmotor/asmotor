a80eb9a39e6b24081cba75676485b12ed76ce0a6
quorum <micjph@protonmail.com>
2022-04-15 11:51:34 +0200

    Update Windows install script

--- install.ps1
+++ install.ps1
@@ -1,13 +1,20 @@
-#!/bin/sh
 $DESTDIR="$home"
+$REPO_URL="https://github.com/asmotor/asmotor.git"
 
-New-Item -Path "$DESTDIR\bin" -Force -ItemType "directory"
-Remove-Item 'build\windows\release' -Recurse
-New-Item -Path 'build\windows\release' -Force -ItemType "directory"
+$null = New-Item -Path "$DESTDIR\bin" -Force -ItemType "directory"
+git clone $REPO_URL --recurse-submodules
+Set-Location -Path "asmotor"
+# git checkout installwin
+# git submodule update
+
+$null = New-Item -Path "build\windows\release" -Force -ItemType "directory"
 
 Set-Location -Path 'build\windows\release'
-cmake -D ASMOTOR_VERSION=$(Get-Content ..\..\..\build\version) -D CMAKE_INSTALL_PREFIX=$DESTDIR --config Release ..\..\..
+cmake -D ASMOTOR_VERSION=$(Get-Content ..\..\..\build\version) -D CMAKE_INSTALL_PREFIX=$DESTDIR ..\..\..
 Set-Location -Path '..\..\..'
 
 cmake --build build\windows\release --config Release
 cmake --install build\windows\release --config Release
+
+Set-Location -Path '..'
+Remove-Item -Path "asmotor" -Recurse -Force
