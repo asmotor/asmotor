3683a18f5a1c7066c5097ca4f5363941390358d7
Carsten Elton Sørensen <cso@rift.dk>
2024-04-06 11:12:27 +0200

    RISC-V instructions

--- test/riscv/allcodes.asm
+++ test/riscv/allcodes.asm
@@ -2,3 +2,8 @@
 
     add x14,x3,x2
     add a4,gp,sp
+    addi t0,s0,-500
+    and x2,x7,x31
+    and sp,t2,t6
+    andi t0,s0,500
+    auipc t0,$2345
--- xasm/riscv/v_parse_instr.c
+++ xasm/riscv/v_parse_instr.c
@@ -18,6 +18,7 @@
 
 #include "errors.h"
 #include "parse.h"
+#include "parse_expression.h"
 
 #include "section.h"
 #include "v_tokens.h"
@@ -56,6 +57,106 @@ parse_Register(int* reg) {
 }
 
 
+static SExpression*
+parse_Unsigned20(void) {
+	SExpression* expr = parse_Expression(2);
+	if (expr != NULL) {
+		uint32_t high = (1 << 20) - 1;
+		expr = expr_CheckRange(expr, 0, high);
+		if (expr != NULL)
+			return expr_And(expr, expr_Const(high));
+	}
+
+	return NULL;
+}
+
+
+static SExpression*
+parse_Expr12(uint32_t low, uint32_t high) {
+	SExpression* expr = parse_Expression(2);
+	if (expr != NULL) {
+		expr = expr_CheckRange(expr, low, high);
+		if (expr != NULL)
+			return expr_And(expr, expr_Const(0x3FFF));
+	}
+
+	return NULL;
+}
+
+
+static SExpression*
+parse_Signed12(void) {
+	return parse_Expr12(-2048, 2047);
+}
+
+
+static SExpression*
+parse_Unsigned12(void) {
+	return parse_Expr12(0, 4095);
+}
+
+
+static bool
+handle_U(uint32_t opcode, EInstructionFormat fmt) {
+	int rd;
+
+	if (parse_Register(&rd)
+	&&  parse_ExpectComma()) {
+
+		SExpression* imm = parse_Unsigned20();
+		if (imm != NULL) {
+			SExpression* op = 
+				expr_Or(
+					expr_Asl(imm, expr_Const(12)),
+					expr_Const(opcode | rd << 7));
+
+			sect_OutputExpr32(op);
+		}
+		return true;
+	}
+
+	return false;
+}
+
+
+static bool
+handle_I(uint32_t opcode, EInstructionFormat fmt, SExpression* (*parseImm)(void)) {
+	int rd, rs1;
+
+	if (parse_Register(&rd)
+	&&  parse_ExpectComma()
+	&&  parse_Register(&rs1)
+	&&	parse_ExpectComma()) {
+
+		SExpression* imm = parseImm();
+		if (imm != NULL) {
+			SExpression* op = 
+				expr_Or(
+					expr_Asl(imm, expr_Const(20)),
+					expr_Const(opcode | rd << 7 | rs1 << 15)
+				);
+
+			sect_OutputExpr32(op);
+		}
+		return true;
+	}
+
+	return false;
+}
+
+
+static bool
+handle_I_S(uint32_t opcode, EInstructionFormat fmt) {
+	return handle_I(opcode, fmt, parse_Signed12);
+}
+
+
+static bool
+handle_I_U(uint32_t opcode, EInstructionFormat fmt) {
+	return handle_I(opcode, fmt, parse_Unsigned12);
+}
+
+
 static bool
 handle_R(uint32_t opcode, EInstructionFormat fmt) {
 	int rd, rs1, rs2;
@@ -74,18 +175,25 @@ handle_R(uint32_t opcode, EInstructionFormat fmt) {
 
 
 #define OP_R(funct7, funct3, opcode) ((funct7) << 25 | (funct3) << 12 | (opcode)),FMT_R
+#define OP_I(funct3, opcode)         ((funct3) << 12 | (opcode)),FMT_I
+#define OP_U(opcode)                 (opcode),FMT_U
+
 
 static SParser
-g_Parsers[T_V_ADD - T_V_ADD + 1] = {
-	{ OP_R(0x00, 0x00, 0x33), handle_R },	/* ADD */
+g_Parsers[T_V_LAST - T_V_32I_ADD + 1] = {
+	{ OP_R(0x00, 0x00, 0x33), handle_R   },	/* ADD */
+	{ OP_I(      0x00, 0x13), handle_I_S },	/* ADDI */
+	{ OP_R(0x00, 0x07, 0x33), handle_R   },	/* AND */
+	{ OP_I(      0x07, 0x13), handle_I_U },	/* ANDI */
+	{ OP_U(            0x17), handle_U   },	/* AUIPC */
 };
 
 
 bool
 v_ParseIntegerInstruction(void) {
-	if (T_V_ADD <= lex_Context->token.id && lex_Context->token.id <= T_V_ADD) {
+	if (T_V_32I_ADD <= lex_Context->token.id && lex_Context->token.id <= T_V_LAST) {
 		ETargetToken token = lex_Context->token.id;
-		SParser* parser = &g_Parsers[token - T_V_ADD];
+		SParser* parser = &g_Parsers[token - T_V_32I_ADD];
 
 		parse_GetToken();
 
